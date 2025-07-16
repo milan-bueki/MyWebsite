06d3789cb88a055fc29fb90b98ce3caf5aa2ffe8
milan-bueki <milanbueki@gmx.de>
2025-07-16 18:57:49 +0200

    added mobile optimization

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,38 +5,39 @@ import "./Header.css";
 export default function Header() {
   return (
     <header className="header">
-      <p className="header-name">Milan Büki</p>
+      <div className="header-inner">
+        <p className="header-name">Milan Büki</p>
 
-      <div className="header-links">
-        <a
-          href="https://github.com/milan-bueki"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="GitHub"
-        >
-          <FaGithub />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/milan-b%C3%BCki-54575a201/"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="LinkedIn"
-        >
-          <FaLinkedin />
-        </a>
-        <a
-          href="https://tryhackme.com/p/milanb"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="TryHackMe"
->
-      <img
-          src="/tryhackme.png"
-          alt="TryHackMe"
-          style={{ height: "24px", marginTop: "2px" }}
-          />
-        </a>
-
+        <div className="header-links">
+          <a
+            href="https://github.com/milan-bueki"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/milan-b%C3%BCki-54575a201/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://tryhackme.com/p/milanb"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="TryHackMe"
+          >
+            <img
+              src="/tryhackme.png"
+              alt="TryHackMe"
+              style={{ height: "24px", marginTop: "2px" }}
+            />
+          </a>
+        </div>
       </div>
     </header>
   );
